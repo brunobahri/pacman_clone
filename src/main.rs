5d4219d8e1da837f4adf5f6ad5e1b2be0db680e8
brunobahri <brunobahri27@gmail.com>
2024-10-08 22:18:54 -0300

    fix: score infinite bug

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ async fn main() {
     let mut pacman_y = 100.0;
     let mut score = 0;
 
-    // Gerar pílulas aleatoriamente
+    
     let mut pills = vec![];
     let mut rng = thread_rng();
     for _ in 0..20 {
@@ -20,7 +20,7 @@ async fn main() {
     loop {
         clear_background(BLACK);
 
-        // Movimento do Pacman
+        
         if is_key_down(KeyCode::Right) {
             pacman_x += 2.0;
         }
@@ -34,22 +34,22 @@ async fn main() {
             pacman_y += 2.0;
         }
 
-        // Desenhar Pacman
+        
         draw_circle(pacman_x, pacman_y, 15.0, YELLOW);
 
-        // Desenhar Pílulas
+        
         pills.retain(|&(x, y)| {
             draw_circle(x, y, 5.0, WHITE);
             let distance = ((pacman_x - x).powi(2) + (pacman_y - y).powi(2)).sqrt();
             if distance < 15.0 {
-                score += 10; // Aumenta a pontuação quando o Pacman coleta uma pílula
-                false // Remove a pílula coletada
+                score += 10; 
+                false 
             } else {
                 true
             }
         });
 
-        // Desenhar Pontuação
+        
         draw_text(&format!("Pontuação: {}", score), 20.0, 20.0, 30.0, WHITE);
 
         next_frame().await;
